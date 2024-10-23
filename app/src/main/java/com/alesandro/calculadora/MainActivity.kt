ebaf33796496c273f905f0babaed1bf756caf721
Alesandro <116791603+AlesandroQuiros@users.noreply.github.com>
2024-10-23 13:54:52 +0200

    Creado guidelines para la vista

--- app/src/main/java/com/alesandro/calculadora/MainActivity.kt
+++ app/src/main/java/com/alesandro/calculadora/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.alesandro.calculadora
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
+/**
+ * Clase de la actividad principal de la aplicación
+ */
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Función que se ejecuta al crear la actividad
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
+
 }
