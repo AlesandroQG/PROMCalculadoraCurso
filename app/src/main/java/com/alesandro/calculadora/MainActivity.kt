6aa631433165460611ed48329aec941120026991
Alesandro <116791603+AlesandroQG@users.noreply.github.com>
2024-10-26 09:11:42 +0200

    Realizado que las operaciones funcionen

--- app/src/main/java/com/alesandro/calculadora/MainActivity.kt
+++ app/src/main/java/com/alesandro/calculadora/MainActivity.kt
@@ -36,13 +36,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.bt7.setOnClickListener(this)
         binding.bt8.setOnClickListener(this)
         binding.bt9.setOnClickListener(this)
+        binding.btComma.setOnClickListener(this)
         binding.btPlus.setOnClickListener(this)
         binding.btMinus.setOnClickListener(this)
         binding.btMul.setOnClickListener(this)
         binding.btDiv.setOnClickListener(this)
-        binding.btComma.setOnClickListener(this)
-        binding.btClear.setOnClickListener(this)
         binding.btEqual.setOnClickListener(this)
+        binding.btClear.setOnClickListener(this)
     }
 
     /**
@@ -80,32 +80,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             binding.bt9 -> {
                 onNumberPressed("9")
             }
-            binding.btPlus -> {
-                onNumberPressed("+")
-            }
-            binding.btMinus -> {
-                onNumberPressed("0")
-            }
-            binding.btMul -> {
-                onNumberPressed("0")
-            }
-            binding.btDiv -> {
-                onNumberPressed("0")
-            }
             binding.btComma -> {
                 onNumberPressed(",")
             }
-            binding.btClear -> {
-                onNumberPressed("0")
+            binding.btPlus -> {
+                onOperationPressed("+")
+            }
+            binding.btMinus -> {
+                onOperationPressed("-")
+            }
+            binding.btMul -> {
+                onOperationPressed("*")
+            }
+            binding.btDiv -> {
+                onOperationPressed("/")
             }
             binding.btEqual -> {
-                onNumberPressed("0")
+                onEqualPress()
+            }
+            binding.btClear -> {
+                onClearPress()
             }
         }
     }
 
     /**
-     * Función para cuando se pulsa uno de los botones de la calculadora
+     * Función para cuando se pulsa uno de los botones numéricos de la calculadora
      */
     private fun onNumberPressed(number: String) {
         renderScreen(number)
@@ -116,11 +116,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
      * Función que muestra en pantalla el número pulsado
      */
     private fun renderScreen(number: String) {
-        var result = ""
-        if (binding.screen.text == "0" && number != ",") {
-            result = number
+        val result = if (binding.screen.text == "0" && number != ",") {
+            number
         } else {
-            result = "${binding.screen.text}${number}"
+            "${binding.screen.text}${number}"
         }
         binding.screen.text = result
     }
@@ -134,6 +133,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         } else {
             secondNumber = binding.screen.text.toString().toDouble()
         }
+    }
+
+    /**
+     * Función para cuando se pulsa uno de los botones de las operaciones de la calculadora
+     */
+    private fun onOperationPressed(operation: String) {
+        this.operation = operation
+        firstNumber = binding.screen.text.toString().toDouble()
+        binding.screen.text = "0"
+    }
+
+    /**
+     * Función para cuando se pulsa el botón del igual de la calculadora
+     */
+    private fun onEqualPress() {
+        val result = when(operation) {
+            "+" -> firstNumber + secondNumber
+            "-" -> firstNumber - secondNumber
+            "*" -> firstNumber * secondNumber
+            "/" -> firstNumber / secondNumber
+            else -> 0
+        }
+        operation = null
+        firstNumber = result.toDouble()
+        secondNumber = 0.0
+        binding.screen.text = if (result.toString().endsWith(".0")) {
+            result.toString().replace(".0", "")
+        } else {
+            "%.2f".format(result)
+        }
+    }
+
+    /**
+     * Función para cuando se pulsa el botón de clear de la calculadora
+     */
+    private fun onClearPress() {
+        //
     }
 
 }
